b6712315e87de0753390d81b9766c20b0f7b1dc6
ianlikono <ianlikono@gmail.com>
2018-04-16 04:05:32 +0300

    optimization

--- dictionary.py
+++ dictionary.py
@@ -8,6 +8,10 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
         yn = input("Did You Mean %s instead? Enter Y if yes or N if no " %
                    get_close_matches(word, data.keys())[0])
